fad8a69623cf35d339a6742c55d218641006a437
JEROME <jerome.developer.tw@gmail.com>
2016-12-19 09:14:07 +0800

    初步完成跳轉到結果詳情頁面。

--- app/src/main/java/com/example/jerome/helloformapp/DisplayTextActivity.java
+++ app/src/main/java/com/example/jerome/helloformapp/DisplayTextActivity.java
@@ -1,23 +1,28 @@
 package com.example.jerome.helloformapp;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.ViewGroup;
-import android.widget.TextView;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+import android.widget.ScrollView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class DisplayTextActivity extends AppCompatActivity {
+
+    @BindView(R.id.resultEditText)
+    EditText resultEditText;
+    @BindView(R.id.activity_display_text)
+    ScrollView activityDisplayText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_text);
+        ButterKnife.bind(this);
         Intent intent = getIntent();
         String text = intent.getStringExtra(MainActivity.EXTRA_TEXT);
-        TextView textView = new TextView(this);
-        textView.setTextSize(40);
-        textView.setText(text);
-        ViewGroup layout = (ViewGroup) findViewById(R.id.activity_display_text);
-        layout.addView(textView);
+        resultEditText.setText(text);
     }
 }
